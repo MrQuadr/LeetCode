197514250c126ee9ec62280715ffa4bd0b64074d
Sergey R <sergay.rom@gmail.com>
2024-11-09 00:42:42 +0300

    create 1829 problem

---
+++ 1829_problem.py
@@ -0,0 +1,32 @@
+# 1829. Maximum XOR for Each Query -> https://leetcode.com/problems/maximum-xor-for-each-query/description/
+
+class Solution:
+
+    def __init__(self) -> None:
+        pass
+
+    def getMaximumXor(self, nums: list[int], maximumBit: int) -> list[int]:
+        maximum = (1 << maximumBit) - 1
+        answer = []
+
+        for num in nums:
+            maximum ^= num
+            answer.append(maximum)
+
+        answer.reverse()
+        return answer
+
+if __name__ == "__main__":
+    test = Solution()
+    nums = [0,1,1,3]
+    maximumBit = 2
+    print(test.getMaximumXor(nums, maximumBit))
+
+    nums = [2,3,4,7]
+    maximumBit = 3
+    print(test.getMaximumXor(nums, maximumBit))
+
+    nums = [0,1,2,2,5,7]
+    maximumBit = 3
+    print(test.getMaximumXor(nums, maximumBit))
+    
